99b30d0afc344c13eccc2a61dfed151d91647eba
robinsonjnr <obinnarobinson1@gmail.com>
2023-01-25 10:51:52 +0000

    python classes

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+"""Define a class Square."""
+
+
 class Square:
 	"""Represent a square."""
 
 	def __init__(self, size):
 		"""Initialize a new square.
+
 		Args:
 			size (int): The size of the new square.
 		"""
@@ -20,9 +24,11 @@ class Square:
 		elif value < 0:
 			raise ValueError("size must be >= 0")
 		self.__size = value
+
 	def area(self):
 		"""Return the current area of the square."""
 		return (self.__size * self.__size)
+
 	def my_print(self):
 		"""Print the square with the # character."""
 		for i in range(0, self.__size):
