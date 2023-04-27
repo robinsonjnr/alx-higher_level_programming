3e903f5097e5f2303d743965ced94ebf738a433d
Obinna Robinson <obinnarobinson1@gmail.com>
2023-04-27 02:33:58 -0700

    task o

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 # Take URL as input and send request and store response in a variable
-
-read -p "Enter URL: " url
-response=$(curl -s $url)
-size=$(echo -n "$response" | wc -c)
-echo "Size of response body: $size bytes"
+curl -s $1 | wc -c | awk -v size=$(curl -sI $1 | awk
 
