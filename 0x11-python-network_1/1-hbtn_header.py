bd339bb0fb84cdba4757b6ffb7857255ce3396b4
Obinna Robinson <obinnarobinson1@gmail.com>
2023-04-28 03:18:15 -0700

    task 0 and 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,6 +6,7 @@ Python script that takes in a URL, sends a request to the URL and displays the v
 import urllib.request
 import sys
 
+if __name__ == "__main__":
 url = sys.argv[1]
 
 with urllib.request.urlopen(url) as response:
